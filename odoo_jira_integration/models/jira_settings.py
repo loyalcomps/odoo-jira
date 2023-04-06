dac98dedfa4fb20699f0804808d90a941dc4a901
SHAMNAS KOYANI <shamnaskoyani@gmail.com>
2023-04-06 12:40:18 +0530

    added check_connection in jira_settings.py

--- odoo_jira_integration/models/jira_settings.py
+++ odoo_jira_integration/models/jira_settings.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import base64
 from odoo import api, fields, models
-
+import requests
 
 class JiraSettings(models.TransientModel):
     _name = 'jira.settings'
@@ -27,3 +28,29 @@ class JiraSettings(models.TransientModel):
             'username': username,
             'api_token': api_token,
         })
+
+    @api.multi
+    def check_connection(self):
+        # Get the Jira URL, username, and API key from the current settings record
+        jira_url = self.jira_url
+        jira_user = self.jira_user
+        jira_api_key = self.jira_api_key
+
+        # Construct the Jira authentication headers using the API key
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(
+                (jira_user + ':' + jira_api_key).encode('utf-8')).decode('utf-8'),
+            'Content-Type': 'application/json'
+        }
+
+        # Make a test request to Jira to check the connection
+        url = jira_url.rstrip('/') + '/rest/api/latest/myself'
+        response = requests.get(url, headers=headers)
+
+        # Check the response status code to determine if the connection was successful
+        if response.status_code == 200:
+            # Connection successful
+            self.message = "Connection successful!"
+        else:
+            # Connection failed
+            self.message = "Connection failed: " + response.reason

faf449b127704ec6b99de00d462c43bce22d8877
SHAMNAS KOYANI <shamnaskoyani@gmail.com>
2023-04-06 12:15:27 +0530

    variable inconsistency cleared

--- odoo_jira_integration/models/jira_settings.py
+++ odoo_jira_integration/models/jira_settings.py
@@ -7,9 +7,9 @@ class JiraSettings(models.TransientModel):
     _name = 'jira.settings'
     _description = 'Jira Settings'
 
-    url = fields.Char(string='Jira URL')
-    username = fields.Char(string='Jira Username')
-    api_token = fields.Char(string='Jira API Token')
+    jira_url = fields.Char(string='Jira URL')
+    jira_user = fields.Char(string='Jira Username')
+    jira_api_token = fields.Char(string='Jira API Token')
 
     @api.model
     def get_settings(self):
